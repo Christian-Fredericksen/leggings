ea5956766c3c23d12e3ab0d9eb73e2c55462324b
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-11-29 16:33:25 -0800

    added carts_controller methods

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,34 @@
 class CartsController < ApplicationController
+    def new
+        cart = Cart.new
+    end
+
+    def create
+        cart = Cart.new(cart_params)
+        if cart.save
+            render json: cart, except: [:created_at, :updated_at]
+        else
+            render json: {message: "cart Failed"}
+        end
+    end
+
+    def index
+        user_id = params[:user_id]
+        user = User.find(user_id)
+        cart = user.cart
+        render json: cart, include: [:game]
+    end
+
+    def destroy
+        cart_id = params[:id]
+        cart = Cart.find(cart_id)
+        cart.destroy
+    end
+
+
+private
+    def cart_params
+      params.require(:cart).permit(:user_id, :game_id)
+    end
+end
 end
