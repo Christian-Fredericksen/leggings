99508f6be2ba1eec755e5f37ac016982582c6a30
christian_fredericksen <christian.fredericksen@yahoo.com>
2020-11-29 16:29:24 -0800

    added sessions controller and routes

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,20 @@
+class SessionController < ApplicationController
+    def login
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            token = encode_token(user.id)
+            render json: {user: UserSerializer.new(user), token: token}
+        else
+            render json: {errors: "Either your username or password was incorrect. Please Try again."}
+        end
+    end
+    def autologin
+        user = session_user
+        if user
+            token = encode_token(user.id)
+            render json: {user: UserSerializer.new(user), token: token}
+        else
+            render json: {errors: "Dont try and hack me. I see YOU!"}
+        end
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :carts
   resources :items
   resources :users
+
+  post '/login', to: 'sessions#login'
+  post '/autologin', to: 'sessions#autologin'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
